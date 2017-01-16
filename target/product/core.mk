48ff9935bd7fdf13153fd43efbc445760a0046fe
espes <espes@pequalsnp.com>
2017-01-16 12:57:11 -0800

    NEOSSetup

--- target/product/core.mk
+++ target/product/core.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     Telecom \
     TeleService \
     VpnDialogs \
-    MmsService
+    MmsService \
+    NEOSSetup
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
